a842e593f8b39112cdd8543929149fb7a26b0a81
Carol Diniz <CarolDinizc21@gmail.com>
2024-07-22 07:32:35 -0300

    Update onMessageCreate.ts

--- src/events/on-MessageCreate/onMessageCreate.ts
+++ src/events/on-MessageCreate/onMessageCreate.ts
@@ -4,16 +4,17 @@ import { ModelGuild } from "../../models/modelGuild";
 import { salesReport } from "./sales-report/salesReport";
 export const logger = new Logger(__filename);
 export async function onMessageCreate(message: Message) {
-  logger.setUserName(message.author.displayName)
-  logger.setGuildName(message.guild!.name)
+  logger.setUserName(message.author.displayName);
+  logger.setGuildName(message.guild!.name);
   try {
     if (message.author.bot) return;
+    console.log(message.content);
 
-    const dbGuild = await ModelGuild.findOne({guildId: message.guildId})
+    const dbGuild = await ModelGuild.findOne({ guildId: message.guildId });
     if (!dbGuild) return;
 
     if (message.channelId === dbGuild.salesReportChannelId) {
-      await salesReport(message)
+      await salesReport(message);
     }
   } catch (error) {}
 }
